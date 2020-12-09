7ef483653c3dab04e9699cfd9cf467d95ed48d2c
SonTrungTo <son.trung.to@gmail.com>
2020-12-09 22:53:27 +0200

    solving 4.8...

---
+++ tests/test_helper.js
@@ -0,0 +1,35 @@
+const Blog = require('../models/blog');
+
+const initialBlogs = [
+    {
+        title: 'Programming is easy',
+        author: 'Son To',
+        url: 'http://localhost:3000/',
+        likes: 0
+    },
+    {
+        title: 'We know nothing about intelligence',
+        author: 'Michael Jordan',
+        url: 'https://michael_jordan@berkeley.com',
+        likes: 100
+    }
+];
+
+const nonExistingId = async () => {
+    const blog = new Blog({
+        title: 'This blog will be deleted soon'
+    });
+    await blog.save();
+    await blog.remove();
+
+    return blog._id.toString();
+};
+
+const blogsInDB = async () => {
+    const blogs = await Blog.find({});
+    return blogs.map(blog => blog.toJSON());
+};
+
+module.exports = {
+    initialBlogs, nonExistingId, blogsInDB
+};
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 const config = {
-    MONGO_URI: process.env.MONGO_URI,
+    MONGO_URI: process.env.NODE_ENV === 'test' ?
+        process.env.TEST_MONGO_URI : process.env.MONGO_URI,
     PORT: process.env.PORT
 };
 
