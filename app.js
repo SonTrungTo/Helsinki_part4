9115454e9e51d3d80d42b3ba115078f1f0c997d2
SonTrungTo <son.trung.to@gmail.com>
2020-12-16 02:32:36 +0200

    added resetRouter and configs in package.json
    ...for exercise 5.17-5.22

--- app.js
+++ app.js
@@ -35,6 +35,11 @@ app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
 
+if (process.env.NODE_ENV === 'test') {
+    const resetRouter = require('./controllers/reset');
+    app.use('/api/reset', resetRouter);
+}
+
 app.use(middleware.unknownEndpoints);
 app.use(middleware.errorHandler);
 
---
+++ controllers/reset.js
@@ -0,0 +1,12 @@
+const resetRouter = require('express').Router();
+const User = require('../models/user');
+const Blog = require('../models/blog');
+
+resetRouter.post('/', async (req, res) => {
+    await User.deleteMany({});
+    await Blog.deleteMany({});
+
+    return res.status(204).end();
+});
+
+module.exports = resetRouter;
